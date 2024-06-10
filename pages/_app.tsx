787ab3d5b9d77c5740e98bb51b054c641ad9ffbc
Vheüel <vheuels@gmail.com>
2024-06-10 21:39:34 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,8 +35,8 @@ export default function({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#7bf8ff" />
+        <meta name="theme-color" content="#7bf8ff" />
       </Head>
       <Component {...pageProps} />
       <Analytics />
