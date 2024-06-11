11d3d9def7be833038ae1375ad0a0257a6e01fe8
Vheüel <vheuels@gmail.com>
2024-06-11 11:44:37 +0700

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -111,7 +111,7 @@ export default function App() {
 	}, [server, email, password, setSession, setUser, setLoggingIn, setLoginError])
 
 	return (
-		<div className="max-w-sm mx-auto w-screen h-screen flex flex-col items-center my-5">
+		<div className="w-screen h-screen flex flex-col items-center my-5">
 			<div className="flex flex-col mx-5 md:w-1/2 xl:w-1/3 overflow-scroll">
 				<div className="flex flex-row mb-1">
 					<div>
